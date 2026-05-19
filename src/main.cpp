52fe769eff6a907568607bc1bacf55635d1997f2
W.Allbri <wes.allbritain@gmail.com>
2026-05-19 17:43:05 -0500

    simple calculator(working), addition and subtraction

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,22 @@
 
 int main(int argc, char** argv){
 
-    std::cout<<"Running... "<<std::endl;
+    std::cout<<"Pleas enter an expression(we can handle + and -): ";
+
+    int lval = 0;
+    int rval;
+    char op;
+    int res;
+
+    std::cin>>lval>>op>>rval;
+
+    if(op=='+')
+        res=lval+rval;
+    else if(op=='-')
+        res=lval-rval;
+
+    std::cout<<"Result: "<<res<<'\n';
+    keep_window_open();
 
     return 0;
 }
--- src/utilities.cpp
+++ src/utilities.cpp
@@ -1,1 +1,9 @@
 #include "utilities.h"
+#include<iostream>
+#include<limits>
+
+void keep_window_open(){
+    std::cout<<"Press Enter to exit... ";
+    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+    std::cin.get();
+}
