2c9c267920d21809912e58affd35a94dd0bfc3d1
W.Allbri <wes.allbritain@gmail.com>
2026-05-23 14:51:30 -0500

    reconfigure main loop

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -1,19 +1,24 @@
 #include <iostream>  
-//#include <vector>
 #include <exception>
 
 #include "utilities.h"
-//#include "token.h"
 #include "i_expression.h"
+#include "token.h"
+#include "token_stream.h"
 
+Token_stream ts;
 
-
-int main(int argc, char** argv){
+int main(/*int argc, char** argv*/){
 
     try{
-        while(std::cin)
-            std::cout<<i_expression()<<'\n';
-        keep_window_open();
+        while(std::cin){
+            std::cout<<'>';
+            Token t = ts.get();
+            while(t.kind()==';')t=ts.get();
+            if(t.kind()=='q') {keep_window_open(); return 0;}
+            ts.putback(t);
+            std::cout<<"="<<i_expression()<<std::endl;
+        }
     }
     catch(std::exception& e){
         std::cerr<<e.what()<<std::endl;
